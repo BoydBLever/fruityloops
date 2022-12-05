5a14c2a57e43e12ed1058894fb1d95ee56a20c34
Boyd Lever <boydlever@stanfordhacker.com>
2022-12-05 10:57:00 -0800

    Update to HomeController

--- src/main/java/com/boydlever/fruityloops/HomeController.java
+++ src/main/java/com/boydlever/fruityloops/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class ItemController {
+public class HomeController {
 
 	@RequestMapping("/")
 	public String index(Model model) {
