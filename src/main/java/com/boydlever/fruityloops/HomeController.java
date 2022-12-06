4b71b35d22040560d6f6ce7ae1d1aee392531391
Boyd Lever <boydlever@stanfordhacker.com>
2022-12-05 21:12:52 -0800

    Update

--- src/main/java/com/boydlever/fruityloops/HomeController.java
+++ src/main/java/com/boydlever/fruityloops/HomeController.java
@@ -10,23 +10,15 @@ import com.boydlever.models.Fruit;
 @Controller
 public class HomeController {
 
-	@RequestMapping("/")
-	public String fruityLoop(Model model) {
-
-		ArrayList<Fruit> fruitList = new ArrayList<Fruit>();
-		
-		fruitList.add(new Fruit("Kiwi", 2.0));
-		fruitList.add(new Fruit("Mango", 2.0));
-		fruitList.add(new Fruit("Goji Berries", 2.0));
-		fruitList.add(new Fruit("Guava", 2.0));
-		fruitList.add(new Fruit("Avocado", 2.0));
-		fruitList.add(new Fruit("Georgia peaches", 2.0));
-
-
-		return "fruityLoop.jsp";
-	}
 	@RequestMapping("/fruityLoop")
-	public String fruityLoop() {
+	public String fruityLoop(Model model) {
+		ArrayList<Fruit>fruitList = new ArrayList<Fruit>();
+		fruitList.add(new Fruit("Kiwi", 5));
+		fruitList.add(new Fruit("Apple", 4));
+		fruitList.add(new Fruit("Orange", 5));
+		fruitList.add(new Fruit("Avocado", 2));
+		
+		model.addAttribute("fruitList", fruitList);
 		return "fruityLoop.jsp";
 	}
 }
--- src/main/webapp/WEB-INF/fruityLoop.jsp
+++ src/main/webapp/WEB-INF/fruityLoop.jsp
@@ -1,5 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <!DOCTYPE html>
 <html>
 <head>
@@ -8,5 +9,20 @@
 </head>
 <body>
 <h1>Fruity Loop</h1>
+<table>
+	<thead>
+		<tr>
+			<th> Name </th>
+			<th> Price </th>
+		</tr>
+	</thead>
+	<tbody>
+		<c:forEach var="eachFruit" items="${fruitLIst}"/>
+			<tr>
+				<td>${name}</td>
+				<td>${price}</td>
+			</tr>
+	</tbody>
+</table>
 </body>
 </html>
